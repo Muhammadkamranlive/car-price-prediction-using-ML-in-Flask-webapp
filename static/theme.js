1e701ac12cdb2f73c622669f296662852e1ef0b9
Muhammadkamranlive <89290897+Muhammadkamranlive@users.noreply.github.com>
2021-08-22 18:34:27 +0500

    First Commit
    Car Price Prediction With Stunning UI

---
+++ static/theme.js
@@ -0,0 +1,133 @@
+(function($) {
+    'use strict';
+	
+	// Mobile Menu
+    $('.mobile-menu nav').meanmenu({
+        meanScreenWidth: "990",
+        meanMenuContainer: ".mobile-menu",
+        onePage: false,
+    });	
+	
+    // counterUp
+    $('.countr_text h1').counterUp({
+        delay: 10,
+        time: 1000
+    });
+	// sticky
+	var wind = $(window);
+	var sticky = $('#sticky-header');
+	wind.on('scroll', function () {
+		var scroll = wind.scrollTop();
+		if (scroll < 100) {
+			sticky.removeClass('sticky');
+		} else {
+			sticky.addClass('sticky');
+		}
+	});
+	
+    /* Portfolio Isotope  */
+    $('.image_load').imagesLoaded(function() {
+
+        if ($.fn.isotope) {
+
+            var $portfolio = $('.image_load');
+
+            $portfolio.isotope({
+
+                itemSelector: '.grid-item',
+
+                filter: '*',
+
+                resizesContainer: true,
+
+                layoutMode: 'masonry',
+
+                transitionDuration: '0.8s'
+
+            });
+            $('.menu-filtering li').on('click', function() {
+
+                $('.menu-filtering li').removeClass('current_menu_item');
+
+                $(this).addClass('current_menu_item');
+
+                var selector = $(this).attr('data-filter');
+
+                $portfolio.isotope({
+
+                    filter: selector,
+
+                });
+
+            });
+
+        };
+
+    });
+	
+		    // Venubox
+
+    $('.venobox').venobox({
+
+        numeratio: true,
+
+        infinigall: true
+
+    });
+
+ // Portfolio Active
+    $('.portfolio_list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: true,
+        nav: false,
+        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            },
+            1000: {
+                items: 5
+            },
+            1920: {
+                items: 5
+            }
+        }
+    })	
+	
+ // Testimonial Active
+    $('.testimonial_list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: true,
+        nav: false,
+        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            },
+            1920: {
+                items: 3
+            }
+        }
+    })	
+
+
+})(jQuery);
